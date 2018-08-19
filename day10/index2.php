010570227ee9e8f96f21668c5a713474d08e9706
datdo <dathalongbay@gmail.com>
2018-08-19 15:52:17 +0700

    table

--- day10/index2.php
+++ day10/index2.php
@@ -1,10 +1,23 @@
 <?php
 $a = 5;
 $b = 3;
+
+// tong
 $tong = $a + $b;
+
+// hieu
 $hieu = $a - $b;
+
+// tich
 $tich = $a*$b;
+
+// thuong
 $thuong = $a/$b;
+
+// comment 1 dong
+/*
+ * Comment nhieu dong
+ */
 $mod = $a%$b;
 echo '<br> tong : ' . $tong;
 echo '<br> hieu : ' . $hieu;
