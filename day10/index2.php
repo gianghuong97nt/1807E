052f3fc6ad06202c09b4910ae975abecca17dfef
datdo <dathalongbay@gmail.com>
2018-08-19 15:34:24 +0700

    table

---
+++ day10/index2.php
@@ -0,0 +1,13 @@
+<?php
+$a = 5;
+$b = 3;
+$tong = $a + $b;
+$hieu = $a - $b;
+$tich = $a*$b;
+$thuong = $a/$b;
+$mod = $a%$b;
+echo '<br> tong : ' . $tong;
+echo '<br> hieu : ' . $hieu;
+echo '<br> tich : ' . $tich;
+echo '<br> thuong : ' . $thuong;
+echo '<br> phep chia lay du : ' . $mod;
